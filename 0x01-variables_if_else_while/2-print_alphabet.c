12118b28318b8f5f5058cd15e2c0ef6504970d8f
Abolade Emmanuel <boladesoladoye@gmail.com>
2022-03-11 10:09:21 -0800

    Lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  main - main block
+ *  Description: Prints the alphabet in lower case folowed by a new line
+ *  Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	for (c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+
+		putchar('\n');
+		return (0);
+}
