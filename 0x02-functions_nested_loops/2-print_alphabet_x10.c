7170c5d3e565b3b63bc81fe1c54e7c6b36659c80
devbolade <boladesoladoye@gmail.com>
2022-03-15 22:50:07 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - print the alphabet
+ * print_alphabet_x10 - it prints alphabet 10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+		int i;
+
+		for (i = 0; i  < 10; i++)
+
+		{
+			char = c;
+
+			for (c = 'a'; c <= 'z'; c++)
+
+			{
+				_putchar(c);
+			}
+
+			_putchar('\n');
+
+		}
+}
