e1123b8738be46979b945a9e2bfe24f57e51e82f
devbolade <boladesoladoye@gmail.com>
2022-03-15 21:26:59 +0000

    put char fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,20 @@
-#include <unistd.h>
+#include <_putchar.h>
 /**
- * _putchar - writes the character to stdout
+ * main - prints the string "_putchar"
  * _putchar -  the character to print
  * Return: 0
  */
-int _putchar(char _putchar)
+int main(void)
 {
+	char, c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+
+	_putchar('\n');
+
 	return (0);
 }
