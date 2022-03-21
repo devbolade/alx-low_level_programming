70a4f2f6f170c6f1cd8f62eec383e99840a6175c
Abolade Emmanuel <boladesoladoye@gmail.com>
2022-03-21 09:08:56 -0700

    Reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,19 +14,19 @@ void rev_string(char *s)
 		while (s[i])
 			i++;
 
-			j = i / 2;
-			right = 0;
+		j = i / 2;
+		right = 0;
 
-			while (right != j)
+		while (right != j)
 
-			{
-				char temp = s[right];
-				int left = i - right - 1;
+		{
+			char temp = s[right];
+			int left = i - right - 1;
 
 			s[right] = s[left];
 
 			s[left] = temp;
 
 			right++;
-			}
+		}
 }
