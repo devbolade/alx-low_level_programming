dcf54eb6811e7ca8f5f69a8384afca956d9fae7f
Abolade Emmanuel <boladesoladoye@gmail.com>
2022-04-04 11:56:42 -0700

    Work

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -24,15 +24,15 @@ char *_strdup(char *str)
 		('\0');
 
 	while (*(str + length_of_string) != '\0')
-		length_of_string++;
-			alloc_mem = malloc(sizeof(*str) * length_of_string);
+	length_of_string++;
+	alloc_mem = malloc(sizeof(*str) * length_of_string);
 
 	if (alloc_mem == NULL)								return
 		('\0');
 
 	for (i = 0; i < length_of_string; i++)
-		alloc_mem[i] = *(str + i);
-			alloc_mem[i] = '\0';
+	alloc_mem[i] = *(str + i);
+	alloc_mem[i] = '\0';
 	return
 		(alloc_mem);
 }
