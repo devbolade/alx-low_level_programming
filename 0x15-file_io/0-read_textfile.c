b714352542c9b89a36d3d435d1bd3bede468cf13
Abolade Emmanuel <boladesoladoye@gmail.com>
2022-05-02 04:06:23 -0700

    First

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/*
+ * read_textfile - read the text in the files.
+ * @filename: file to read and write
+ * @letters: number of letters to read and write.
+ * @read_textfile: read the text file for the text.
+ * Return: letters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+		ssize_t nletters;
+		int file;
+		char *text;
+
+		if (!filename)
+			return (0);
+		text = malloc(sizeof(char) * letters + 1);
+		if (text == NULL)
+			return (0);
+		file = open(filename, O_RDONLY);
+		if (file == -1)
+		{
+			free(text);
+			return (0);
+		}
+		nletters = read(file, text, sizeof(char) * letters);
+		if (nletters == -1)
+		{
+			free(text);
+			close(file);
+			return (0);
+		}
+		nletters = write(STDOUT_FILENO, text, nletters);
+		if (nletters == -1)
+		{
+			free(text);
+			close(file);
+			return (0);
+		}
+		free(text);
+		close(file);
+		return (nletters);
+}
