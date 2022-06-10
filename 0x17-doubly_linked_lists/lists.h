b876469a315bd423080d22b393aa643fbdaa7fbe
Devbolade <boladesoladoye@gmail.com>
2022-06-10 06:01:58 -0700

    Function that returns the number of elements in a linked dlistint_t list

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,29 +1,17 @@
 #ifndef _LISTS_H_
-
 #define _LISTS_H_
-
-
-
 #include <stdlib.h>
-
 #include <stdio.h>
 
 
 
 /*
- *
  * struct dlistint_s - doubly linked list
- *
  * @n: integer
- *
  * @prev: points to the previous node
- *
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- *
- *
- *
  */
 
 
